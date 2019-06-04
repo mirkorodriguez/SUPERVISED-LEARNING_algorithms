fade92c0552d63e2f0209c96a0fabe296ed525b7
Mirko Rodriguez <mirko.rodriguezm@gmail.com>
2019-06-03 23:55:12 -0500

    Multilayer Feed Forward Neural Network implementation by using Tensorflow and Keras for Classification.

---
+++ ArtificialNeuralNetworks/MultilayerFeedforwardNeuralNetwork/Classification/MLFFN-Plain/mlffn-main.py
@@ -0,0 +1,142 @@
+
+#-------------------------------------
+# PARTE I - Preprocesamiento de la Data
+#-------------------------------------
+
+# Importando librerías
+import pandas as pd
+import numpy as np
+from util import plot_confusion_matrix, saveFile, plotHistogram, plotCorrelations
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+
+# Importando Datasets
+dataset_csv = pd.read_csv('../dataset/clientes_data.csv')
+
+# Describir la data original
+print ("\nDataset original:\n", dataset_csv.describe(include='all'))
+
+# Dataset reducido
+dataset = dataset_csv.iloc[:,3:14]
+dataset_columns = dataset.columns
+dataset_values = dataset.values
+
+# Describir la data truncada
+print ("\nDataset reducido: \n", dataset.describe(include='all'))
+print("\n",dataset.head())
+
+# Codificando datos categóricos
+labelEncoder_X_1 = LabelEncoder()
+dataset_values[:, 1] = labelEncoder_X_1.fit_transform(dataset_values[:, 1])
+labelEncoder_X_2 = LabelEncoder()
+dataset_values[:, 2] = labelEncoder_X_2.fit_transform(dataset_values[:, 2])
+print ("\nDataset Categorizado: \n", dataset_values)
+
+# Guardando LabelEncoder a disco
+saveFile(labelEncoder_X_1,"./model/labelEncoder_X_1.save")
+saveFile(labelEncoder_X_2,"./model/labelEncoder_X_2.save")
+
+# Escalamiento/Normalización de Features (StandardScaler: (x-u)/s)
+stdScaler = StandardScaler()
+dataset_values[:,0:10] = stdScaler.fit_transform(dataset_values[:,0:10])
+# print("\nStandardScaler: ", stdScaler)
+# print("No Observations: ", stdScaler.n_samples_seen_)
+# print("Mean: ", stdScaler.mean_)
+# print("Varianza: ", stdScaler.var_)
+print ("\nDataset Normalizado\n: ", dataset_values)
+
+# Guardando StandardScaler a disco
+saveFile(stdScaler,"./model/stdScaler.save")
+
+# Dataset final normalizado
+dataset_final = pd.DataFrame(dataset_values,columns=dataset_columns, dtype=np.float64)
+
+print ("\nDataset Final:")
+print(dataset_final.describe(include='all'))
+print("\n", dataset_final.head())
+
+# Distribuciones de la data y Correlaciones
+plotHistogram(dataset_final)
+plotCorrelations(dataset_final)
+
+# Obteniendo valores a procesar
+X = dataset_final.iloc[:, 0:10].values
+y = dataset_final.iloc[:, 10].values
+
+# Dividiendo el Dataset en sets de Training y Test
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+#----------------------------------------------------
+# PARTE II - Construyendo la Red Neuronal Artificial!
+#----------------------------------------------------
+
+# Importando Keras y Tensorflow
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.initializers import RandomUniform
+
+# Inicializando la Red Neuronal
+neural_network = Sequential()
+
+# kernel_initializer Define la forma como se asignará los Pesos iniciales Wi
+initial_weights = RandomUniform(minval = -0.5, maxval = 0.5)
+
+# Agregado la Capa de entrada y la primera capa oculta
+# 10 Neuronas en la capa de entrada y 8 Neuronas en la primera capa oculta
+neural_network.add(Dense(units = 8, kernel_initializer = initial_weights, activation = 'relu', input_dim = 10))
+
+# Agregando capa oculta
+neural_network.add(Dense(units = 5, kernel_initializer = initial_weights, activation = 'relu'))
+
+# Agregando capa oculta
+neural_network.add(Dense(units = 4, kernel_initializer = initial_weights, activation = 'relu'))
+
+# Agregando capa de salida
+neural_network.add(Dense(units = 1, kernel_initializer = initial_weights, activation = 'sigmoid'))
+
+# Imprimir Arquitectura de la Red
+neural_network.summary()
+
+# Compilando la Red Neuronal
+# optimizer: Algoritmo de optimización | binary_crossentropy = 2 Classes
+# loss: error
+neural_network.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+
+
+# Entrenamiento
+neural_network.fit(X_train, y_train, batch_size = 32, epochs = 100)
+
+
+
+
+#-----------------------------------------------
+# PARTE III - Predicciones y evaluando el modelo
+#-----------------------------------------------
+
+#standarScaler = joblib.load("./model/stdScaler.save")
+#print("StandardScaler: ",standarScaler)
+#print("No Observations: ", standarScaler.n_samples_seen_)
+#print("Mean: ", standarScaler.mean_)
+#print("Varianza: ", standarScaler.var_)
+
+# Haciendo predicción de los resultados del Test
+y_pred = neural_network.predict(X_test)
+y_pred_norm = (y_pred > 0.5)
+
+y_pred_norm = y_pred_norm.astype(int)
+y_test = y_test.astype(int)
+
+# 50 primeros resultados a comparar
+print("\nPredicciones (50 primeros):")
+print("\n\tReal", "\t", "Predicción(N)","\t", "Predicción (O)")
+for i in range(50):
+    print(i, '\t', y_test[i], '\t ', y_pred_norm[i], '\t \t', y_pred[i])
+
+# Aplicando la Matriz de Confusión
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred_norm)
+print ("\nMatriz de Confusión: \n", cm)
+
+# Graficando la Matriz de Confusión
+plot_confusion_matrix(y_test, y_pred_norm, normalize=False,title="Matriz de Confusión: Abandono de clientes")
---
+++ ArtificialNeuralNetworks/MultilayerFeedforwardNeuralNetwork/Classification/MLFFN-Plain/util.py
@@ -0,0 +1,75 @@
+# Funciones
+
+import numpy as np
+
+from sklearn.metrics import confusion_matrix
+from sklearn.utils.multiclass import unique_labels
+from sklearn.externals import joblib
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+def plot_confusion_matrix(y_true, y_pred,
+                          normalize=False,
+                          title=None):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    if not title:
+        if normalize:
+            title = 'Matriz de Confusión Normalizada'
+        else:
+            title = 'Matriz de Confusión sin Normalizar'
+
+    # Compute confusion matrix
+    cm = confusion_matrix(y_true, y_pred)
+    # Only use the labels that appear in the data
+    classes = unique_labels(y_true, y_pred)
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Matriz de Confusión Normalizada")
+    else:
+        print('Matriz de Confusión sin Normalizar')
+
+    print(cm)
+
+    fig, ax = plt.subplots()
+    im = ax.imshow(cm, interpolation='nearest', cmap=plt.cm.Blues)
+    ax.figure.colorbar(im, ax=ax)
+    # We want to show all ticks...
+    ax.set(xticks=np.arange(cm.shape[1]),
+           yticks=np.arange(cm.shape[0]),
+           # ... and label them with the respective list entries
+           xticklabels=classes, yticklabels=classes,
+           title=title,
+           ylabel='True label',
+           xlabel='Predicted label')
+
+    # Rotate the tick labels and set their alignment.
+    plt.setp(ax.get_xticklabels(), rotation=45, ha="right",
+             rotation_mode="anchor")
+
+    # Loop over data dimensions and create text annotations.
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i in range(cm.shape[0]):
+        for j in range(cm.shape[1]):
+            ax.text(j, i, format(cm[i, j], fmt),
+                    ha="center", va="center",
+                    color="white" if cm[i, j] > thresh else "black")
+    fig.tight_layout()
+    plt.show()
+    return ax
+
+def saveFile(object_to_save, scaler_filename):
+    joblib.dump(object_to_save, scaler_filename)
+
+def plotHistogram(dataset_final):
+    dataset_final.hist(figsize=(20,14), edgecolor="black", bins=20)
+    plt.show()
+
+def plotCorrelations(dataset_final):
+    fig, ax = plt.subplots(figsize=(15,12))   # size in inches
+    sns.heatmap(dataset_final.corr(), annot=True, ax=ax)
+    plt.show()
